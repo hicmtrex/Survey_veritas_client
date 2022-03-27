adbcd2b59de6ddbfd8cd3d48f05937d143c79bd4
Firstname Lastname <your_email@youremail.com>
2022-03-28 00:32:20 +0100

    deploy

--- src/axios.js
+++ src/axios.js
@@ -1,12 +1,12 @@
 /**
  * Created by Zura on 12/25/2021.
  */
-import axios from "axios";
-import store from "./store";
-import router from "./router";
+import axios from 'axios';
+import store from './store';
+import router from './router';
 
 const axiosClient = axios.create({
-  baseURL: `http://127.0.0.1:8000/api`,
+  baseURL: `${process.env.VITE_API_BASE_URL}/api`,
 });
 
 axiosClient.interceptors.request.use((config) => {
@@ -20,10 +20,10 @@ axiosClient.interceptors.response.use(
   },
   (error) => {
     if (error.response.status === 401) {
-      sessionStorage.removeItem("TOKEN");
-      router.push({ name: "Login" });
+      sessionStorage.removeItem('TOKEN');
+      router.push({ name: 'Login' });
     } else if (error.response.status === 404) {
-      router.push({ name: "NotFound" });
+      router.push({ name: 'NotFound' });
     }
     return error;
   }
